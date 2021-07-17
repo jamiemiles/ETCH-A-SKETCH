c940ac98c97aa9ebbbe472eee2e6af03538df38e
Jamie Miles <jamiemiles.d@gmail.com>
2021-07-17 21:08:22 +0100

    mouse hover fills each square in black

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ const etchaSketch = () => {
       const grid = document.createElement("div");
       const screen = document.querySelector(".screen");
       grid.className = `square${i}`;
+
       screen.appendChild(grid);
       grid.style = "border: 1px solid black; height: 32px; width: 32px; ";
     }
   };
   const drawSquares = () => {
     for (let i = 0; i < 16 ** 2; i++) {
-      const squarePosition = document.querySelectorAll(`.square${i}`);
-
-      squarePosition.forEach(square, function () {
-        this.style = "background-color: black";
+      const test = document.querySelector(`.square${i}`);
+      test.addEventListener("mouseover", function (e) {
+        test.style = "background-color: black; height: 32px; width: 32px";
+        console.log(test);
       });
     }
   };
